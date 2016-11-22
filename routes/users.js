41a594ecfde7f89a087786f2d8774d8b2f1db868
Alexander <alexander.lock7@gmail.com>
2016-11-22 01:01:18 +0000

    routes and partials

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  console.log("User Profile");
+  res.render('user');
 });
 
 
