fded8bd6ed561542d8f4fd8ccab6b1fce86e96ec
Alexander <alexander.lock7@gmail.com>
2016-11-23 16:33:13 +0000

    home page can load images from bucket

--- routes/index.js
+++ routes/index.js
@@ -13,45 +13,13 @@ const storage = google.storage('v1');
 module.exports = (knex) => {
 
 
-const auth_list = () => {
-
-  return google.auth.getApplicationDefault(function(err, authClient) {
-    if (err) {
-      console.log('Authentication failed because of ', err);
-      return;
-    }
-    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-      const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
-      authClient = authClient.createScoped(scopes);
-    }
-
-    const request = {
-      bucket: "faceimages",
-      auth: authClient
-    };
 
 
-    var recur = function(err, result) {
-      if (err) {
-        console.log(err);
-      }else {
-       //console.log(result.items);
 
-        if (result.nextPageToken) {
-          request.pageToken = result.nextPageToken;
-          storage.objects.list(request, recur);
-        }
-      }
-    };
+  //auth_list();
 
-     storage.objects.list(request, recur);
-  })
-};
-
-
-//auth_list();
-
-const bucket_list = [ { kind: 'storage#object',
+  const bucket_list = [{
+    kind: 'storage#object',
     id: 'faceimages/bolt.jpg/1479747850513000',
     selfLink: 'https://www.googleapis.com/storage/v1/b/faceimages/o/bolt.jpg',
     name: 'bolt.jpg',
@@ -67,8 +35,9 @@ const bucket_list = [ { kind: 'storage#object',
     md5Hash: 'K7YKj/cD2JiXC9EhRPGBeQ==',
     mediaLink: 'https://www.googleapis.com/download/storage/v1/b/faceimages/o/bolt.jpg?generation=1479747850513000&alt=media',
     crc32c: 'EikD1g==',
-    etag: 'COjU8qequtACEAY=' },
-  { kind: 'storage#object',
+    etag: 'COjU8qequtACEAY='
+  }, {
+    kind: 'storage#object',
     id: 'faceimages/demo-image.jpg/1479415026201000',
     selfLink: 'https://www.googleapis.com/storage/v1/b/faceimages/o/demo-image.jpg',
     name: 'demo-image.jpg',
@@ -84,8 +53,9 @@ const bucket_list = [ { kind: 'storage#object',
     md5Hash: 'o4HBFwMl+uhWmfUgOlnCfw==',
     mediaLink: 'https://www.googleapis.com/download/storage/v1/b/faceimages/o/demo-image.jpg?generation=1479415026201000&alt=media',
     crc32c: 'tzJjUw==',
-    etag: 'CKjj8rjSsNACEAI=' },
-  { kind: 'storage#object',
+    etag: 'CKjj8rjSsNACEAI='
+  }, {
+    kind: 'storage#object',
     id: 'faceimages/dunk.jpg/1479415328294000',
     selfLink: 'https://www.googleapis.com/storage/v1/b/faceimages/o/dunk.jpg',
     name: 'dunk.jpg',
@@ -101,8 +71,9 @@ const bucket_list = [ { kind: 'storage#object',
     md5Hash: 'lIGfh3uANHEiy7QCerSbdQ==',
     mediaLink: 'https://www.googleapis.com/download/storage/v1/b/faceimages/o/dunk.jpg?generation=1479415328294000&alt=media',
     crc32c: '4I1EmA==',
-    etag: 'CPCI+cjTsNACEAI=' },
-  { kind: 'storage#object',
+    etag: 'CPCI+cjTsNACEAI='
+  }, {
+    kind: 'storage#object',
     id: 'faceimages/obama.jpg/1479747900747000',
     selfLink: 'https://www.googleapis.com/storage/v1/b/faceimages/o/obama.jpg',
     name: 'obama.jpg',
@@ -118,25 +89,87 @@ const bucket_list = [ { kind: 'storage#object',
     md5Hash: 'BvmTu3ualMVweyBErGvzfA==',
     mediaLink: 'https://www.googleapis.com/download/storage/v1/b/faceimages/o/obama.jpg?generation=1479747900747000&alt=media',
     crc32c: 'uJPyOA==',
-    etag: 'CPjZ7L+qutACEAI=' } ]
+    etag: 'CPjZ7L+qutACEAI='
+  }]
+
+
+
+  router.get('/', function(req, res, next) {
+    console.log("HOME PAGE");
+
+    const get_bucket_promise = new Promise(function(resolve, reject) {
+      google.auth.getApplicationDefault(function(err, authClient) {
+        if (err) {
+          console.log('Authentication failed because of ', err);
+          return;
+        }
+        if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+          const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+          authClient = authClient.createScoped(scopes);
+        }
+
+        const request = {
+          bucket: "faceimages",
+          auth: authClient
+        };
+
+
+        var recur = function(err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            //console.log(result.items);
+            resolve(result.items);
+            if (result.nextPageToken) {
+              request.pageToken = result.nextPageToken;
+              storage.objects.list(request, recur);
+            }
+          }
+        };
+
+        storage.objects.list(request, recur);
+
+      })
+    });
+
+
+
+
+    get_bucket_promise.then(function(val) {
+       var self_links = [];
+
+       val.forEach((photo) => {
+        self_links.push(photo.name);
+      });
+       res.render('index', {
+        data: self_links
+      });
+
+
+      })
+      .catch(function(err) {
+        console.log('Failed to get Bucket items:', err);
+      });
 
 
 
 
 
+   /* var self_links = []
+    bucket_list.forEach((photo) => {
+      self_links.push(photo.name);
+    });*/
 
-router.get('/', function(req, res, next) {
+/*    res.render('index', {
+      data: self_links
+    });*/
 
-  console.log("HOME PAGE");
-  var self_links =[]
-  bucket_list.forEach((photo) => {
-    self_links.push(photo.name);
+
   });
 
-  res.render('index', {data: self_links});
-});
 
-return router;
+
+  return router;
 
 
 
