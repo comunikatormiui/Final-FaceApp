aa741a771f0594366a40886ad48afecff9253d47
Alexander <alexander.lock7@gmail.com>
2016-11-23 19:18:45 +0000

    user page shows pictures they made

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,33 @@ const router = express.Router();
 /* GET users listing. */
 module.exports = (knex) => {
 
-router.get('/:id', function(req, res, next) {
-  console.log("User Profile");
-  res.render('user');
-});
+/*  router.get('/:id', function(req, res, next) {
+      knex
+      .select("*")
+      .from("users")
+      .where("id",req.params.id)
+      .then((results) => {
+           console.log("User Profile");
+           console.log(results);
+           res.render('user', {data:results});
+          })
+  });*/
+
+   router.get('/:id', function(req, res, next) {
+      knex("users")
+      .join('photos', 'users.id', 'photos.user_id')
+      .select('*')
+      .where("user_id", req.params.id)
+      .then((results) => {
+           console.log("User Profile");
+           res.render('user', {data:results});
+          })
+  });
+
+
+
+
+
 
 return router;
 
